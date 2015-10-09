0a4603be0374445187c17226ac92737ded8e1538
wboka <boka.wayne@gmail.com>
2015-10-08 20:55:01 -0400

    Set up for custom color features

--- js/percircle.js
+++ js/percircle.js
@@ -8,18 +8,40 @@ jQuery(document).ready(function () {
 		if( percentage > 50 ){
 			jQuery( this ).addClass( 'exceed' );
 		}
-		render( this, percentage )
+		var barColor = jQuery( this ).data( "barColor" );
+
+		if( typeof barColor === "undefined" || !(new RGBColor(barColor).ok) ){
+			barColor = "black";
+		} else {
+			barColor = new RGBColor(barColor).toRGB();
+		}
+		var bgColor = jQuery( this ).data( "bgColor" );
+
+		if( typeof bgColor === "undefined" || !(new RGBColor(bgColor).ok) ){
+			bgColor = "grey";
+		} else {
+			bgColor = new RGBColor(bgColor).toRGB();
+		}
+		render( this, percentage, barColor, bgColor )
 	});
 
-	function render( node, percentage ){
+	function render( node, percentage, barColor, bgColor ){
 		var rotationMultiplier = 3.6;
 		var rotationDegrees = rotationMultiplier * percentage;
+		var isDark = $( node ).hasClass('dark');
+
 		jQuery( node ).find( '.bar' ).css({
 			'-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
 			'-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
 			'-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
 			'-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
 			'transform'         : 'rotate(' + rotationDegrees + 'deg)'
+		});
+
+		jQuery( node ).find( '.bar, .fill' ).css({
+			'background-color'	: isDark ? bgColor : barColor,
+			'border-color' 		: isDark ? bgColor : barColor,
+			'color'				: isDark ? bgColor : barColor
 		});
 	}
 });

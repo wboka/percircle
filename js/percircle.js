8b9e6c662cc7c60f90705e49fdde175f48ea0bd7
wboka <boka.wayne@gmail.com>
2015-10-11 20:37:54 -0400

    Removes unneeded code

--- js/percircle.js
+++ js/percircle.js
@@ -8,33 +8,33 @@ jQuery(document).ready(function () {
         if( percentage > 50 ){
             jQuery( this ).addClass( 'exceed' );
         }
-        var barColor = jQuery( this ).data( "barColor" );
 
+        var barColor = jQuery( this ).data( "barColor" );
         if( typeof barColor === "undefined" || !(new RGBColor(barColor).ok) ){
             barColor = "black";
         } else {
             barColor = new RGBColor(barColor).toRGB();
         }
-        var bgColor = jQuery( this ).data( "bgColor" );
 
+        var bgColor = jQuery( this ).data( "bgColor" );
         if( typeof bgColor === "undefined" || !(new RGBColor(bgColor).ok) ){
             bgColor = "grey";
         } else {
             bgColor = new RGBColor(bgColor).toRGB();
         }
-        render( this, percentage, barColor, bgColor )
+
+        render( this, percentage, barColor, bgColor );
     });
 
     function render( node, percentage, barColor, bgColor ){
-        var rotationMultiplier = 3.6;
-        var rotationDegrees = rotationMultiplier * percentage;
+        var rotationDegrees = 3.6 * percentage;
         var percircleContent = '<div class="slice"><div class="bar"></div><div class="fill"></div></div>';
 
         jQuery( node ).css('background-color', bgColor);
 
         jQuery( node ).append('<span class="percircle-amount">' + percentage + "%</span>" + percircleContent);
 
-        jQuery ( node ).add(jQuery( 'span', jQuery( node ) ) ).css('color', barColor);
+        jQuery( node ).add(jQuery( 'span', jQuery( node ) ) ).css('color', barColor);
 
         jQuery( node ).find( '.bar' ).css({
             '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
@@ -42,12 +42,6 @@ jQuery(document).ready(function () {
             '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
             '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
             'transform'         : 'rotate(' + rotationDegrees + 'deg)'
-        });
-
-        jQuery( node ).find( '.bar, .fill' ).css({
-            'background-color'    : barColor,
-            'border-color'         : barColor,
-            'color'                : barColor
         });
     }
 });

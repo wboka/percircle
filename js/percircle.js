cf9e5b7c0293d407f5d7ca7cd170e1012314896b
wboka <wboka@users.noreply.github.com>
2015-10-14 11:41:05 -0400

    Moves to a more jQuery plugin friendly format

--- js/percircle.js
+++ js/percircle.js
@@ -1,47 +1,70 @@
-jQuery(document).ready(function () {
+/**
+ *  @author 		Wayne T Boka
+ *  @lastmodified 	201510141130
+ *  @version		0.1.0
+ *  @description	Plugin for percent circle graphs
+ *  @website		http://wboka.github.io/percircle
+ */
 
-    //Identify by data attribute
-    var dataPercircles = jQuery( "div[data-percircle]" );
+(function ($) {
+	var percircleContent = '<div class="slice"><div class="bar"></div><div class="fill"></div></div>';
+	
+	// Define available methods
+    var methods = {
+        init: function () {
+            return this.each(function () {
+            	var percentage = $( this ).data( "percircle" );
+                if( percentage > 50 ){
+                    $( this ).addClass( 'exceed' );
+                }
 
-    jQuery.each( dataPercircles, function(){
-        var percentage = jQuery( this ).data( "percircle" );
-        if( percentage > 50 ){
-            jQuery( this ).addClass( 'exceed' );
+                var barColor = $( this ).data( "barColor" );
+                if( typeof barColor === "undefined" || !(new RGBColor(barColor).ok) ){
+                    barColor = "black";
+                } else {
+                    barColor = new RGBColor(barColor).toRGB();
+                }
+
+                var bgColor = $( this ).data( "bgColor" );
+                if( typeof bgColor === "undefined" || !(new RGBColor(bgColor).ok) ){
+                    bgColor = "grey";
+                } else {
+                    bgColor = new RGBColor(bgColor).toRGB();
+                }
+
+                methods['render']( this, percentage, barColor, bgColor );
+            });
+        },
+        render: function ( node, percentage, barColor, bgColor ) {
+        	var rotationDegrees = 3.6 * percentage;
+
+        	$( node ).css('background-color', bgColor);
+
+            $( node ).append('<span class="percircle-amount">' + percentage + "%</span>" + percircleContent);
+
+            $( node ).add($( 'span', $( node ) ) ).css('color', barColor);
+
+            $( node ).find( '.bar' ).css({
+                '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
+                '-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
+                '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
+                '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
+                'transform'         : 'rotate(' + rotationDegrees + 'deg)'
+            });
+        },
+		destroy: function() {
+			return this.each(function() {
+				$( 'div.slice', $( this )).remove();
+			});
+		}
+    };
+
+    $.fn.percircle = function (method) {
+		// Check if the desired method exists
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
         }
-
-        var barColor = jQuery( this ).data( "barColor" );
-        if( typeof barColor === "undefined" || !(new RGBColor(barColor).ok) ){
-            barColor = "black";
-        } else {
-            barColor = new RGBColor(barColor).toRGB();
-        }
-
-        var bgColor = jQuery( this ).data( "bgColor" );
-        if( typeof bgColor === "undefined" || !(new RGBColor(bgColor).ok) ){
-            bgColor = "grey";
-        } else {
-            bgColor = new RGBColor(bgColor).toRGB();
-        }
-
-        render( this, percentage, barColor, bgColor );
-    });
-
-    function render( node, percentage, barColor, bgColor ){
-        var rotationDegrees = 3.6 * percentage;
-        var percircleContent = '<div class="slice"><div class="bar"></div><div class="fill"></div></div>';
-
-        jQuery( node ).css('background-color', bgColor);
-
-        jQuery( node ).append('<span class="percircle-amount">' + percentage + "%</span>" + percircleContent);
-
-        jQuery( node ).add(jQuery( 'span', jQuery( node ) ) ).css('color', barColor);
-
-        jQuery( node ).find( '.bar' ).css({
-            '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
-            '-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
-            '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
-            '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
-            'transform'         : 'rotate(' + rotationDegrees + 'deg)'
-        });
-    }
-});
+        
+        return methods['init'].apply(this, Array.prototype.slice.call(arguments, 1));
+    };
+})(jQuery);
